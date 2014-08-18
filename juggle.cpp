27f3e259de80966fc45cbc25ed9643237664f46a
Marshall <marshallmassengill@gmail.com>
2014-08-17 22:57:05 -0400

    Update juggle.cpp

--- juggle.cpp
+++ juggle.cpp
@@ -45,9 +45,34 @@ void createPositionTrackbarWindow(){
 	
 }
 
-void manipulateImage(){
-	
+void manipulateImage(cv::Mat &imageIn, cv::Mat &imageOut){
+	cv::Mat temp, gray;
+
+	//cv::GaussianBlur( frame, out, Size(5,5), 3, 3);
+	cv::pyrDown(imageIn, temp);				
+	cv::cvtColor(temp, gray, CV_BGR2HSV);
+	//imshow("MyVideo", frame); //show the frame in "MyVideo" window
+	cv::inRange(gray, cv::Scalar(H_MIN,S_MIN,V_MIN),cv::Scalar(H_MAX,S_MAX,V_MAX),imageOut);
+	//cv::inRange(gray, cv::Scalar(165,105,170),cv::Scalar(230,256,205),imageOut);
+	//morphOps(out);
 }
+
+//rewrite!!!!
+void morphOps(cv::Mat &thresh){
+  //create structuring element that will be used to "dilate" and "erode" image.
+  //the element chosen here is a 3px by 3px rectangle
+
+  cv::Mat erodeElement = getStructuringElement( cv::MORPH_RECT,cv::Size(3,3));
+  //dilate with larger element so make sure object is nicely visible
+  cv::Mat dilateElement = getStructuringElement( cv::MORPH_ELLIPSE,cv::Size(8,8));
+
+  cv::erode(thresh,thresh,erodeElement);
+  cv::erode(thresh,thresh,erodeElement);
+
+  cv::dilate(thresh,thresh,dilateElement);
+  cv::dilate(thresh,thresh,dilateElement);
+}
+
 
 
 int main(int argc, char* argv[]){
@@ -66,7 +91,7 @@ int main(int argc, char* argv[]){
 
   while(1){
 		//Declaring the local variables for the video loop
-    cv::Mat frame, out, temp, gray;
+    cv::Mat frame, output;
 
 		//Reads a new frame from the video and verifies it
     bool readVideoSuccess = video.read(frame);
@@ -76,6 +101,7 @@ int main(int argc, char* argv[]){
     }
 				
 		//Where the image manipulation happens
+		manipulateImage(frame, output);
 
 		//wait for 'esc' key press for 30 ms. If 'esc' key is pressed, break loop
 		if(cv::waitKey(30) == 27){
@@ -85,30 +111,21 @@ int main(int argc, char* argv[]){
 
 
 
-				//cv::GaussianBlur( frame, out, Size(5,5), 3, 3);
-				cv::pyrDown(frame, temp);				
-				cv::cvtColor(temp, gray, CV_BGR2HSV);
-        //imshow("MyVideo", frame); //show the frame in "MyVideo" window
-
-				cv::inRange(gray, cv::Scalar(H_MIN,S_MIN,V_MIN),cv::Scalar(H_MAX,S_MAX,V_MAX),out);
-				cv::inRange(gray, cv::Scalar(165,105,170),cv::Scalar(230,256,205),out);
-				//morphOps(out);
-
 				cv::vector<cv::Vec3f> circles;
 
-				cv::HoughCircles(out, circles, CV_HOUGH_GRADIENT, 1, out.rows/2, 20, 10, 0, 0 );
+				cv::HoughCircles(output, circles, CV_HOUGH_GRADIENT, 1, output.rows/2, 20, 10, 0, 0 );
 
 				for( size_t i = 0; i < circles.size(); i++ )
 				{
 					cv::Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
 					int radius = cvRound(circles[i][2]);
 					// draw the circle center
-					circle( temp, center, 3, cv::Scalar(0,255,0), -1, 8, 0 );
+					circle( output, center, 3, cv::Scalar(0,255,0), -1, 8, 0 );
 					// draw the circle outline
-					circle( temp, center, radius, cv::Scalar(0,0,255), 3, 8, 0 );
+					circle( output, center, radius, cv::Scalar(0,0,255), 3, 8, 0 );
 				}
 
-        cv::imshow("output", out); //show the frame in "MyVideo" window
+        cv::imshow("output", output); //show the frame in "MyVideo" window
 
     }
     return 0;
